7dff69fb504ba971fb1d3685cdae9fcfa87c15d0
Kartik Tomar <kartiktomar112233@gmail.com>
2025-03-15 09:02:14 +0530

    dockerized fe

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  
-})
+  plugins: [react(),tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Allow external access
+    port: 5173, // Ensure it matches the Docker port
+    strictPort: true, // Ensures it fails if the port is unavailable
+  },
+});
